cef5bbc3ebeb651ae2056edbdeb3debc33141655
tmshn <shnch.tmr@gmail.com>
2018-11-28 21:31:48 +0900

    update

--- .config/fish/tools.fish
+++ .config/fish/tools.fish
@@ -6,3 +6,6 @@ set -x EDITOR micro
 set -x FZF_DEFAULT_OPTS '--border --cycle --height 30% --exit-0 --select-1'
 
 alias cat bat
+
+set -x PATH "$HOME/.anyenv/bin" $PATH
+status --is-interactive; and source (anyenv init -|psub)
