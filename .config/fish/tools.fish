196435f64ef907b37afcdf1022d79ef29a56f7dd
tmshn <shnch.tmr@gmail.com>
2018-11-28 20:43:43 +0900

    update

--- .config/fish/tools.fish
+++ .config/fish/tools.fish
@@ -1,7 +1,8 @@
-set -x PATH "$XDG_CONFIG_HOME/../bin" $PATH
 set -x PATH "$HOME/bin" $PATH
 set -x GOPATH $HOME
 
 set -x EDITOR micro
 
 set -x FZF_DEFAULT_OPTS '--border --cycle --height 30% --exit-0 --select-1'
+
+alias cat bat
