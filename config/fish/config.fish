0e180132d9239b1bbc0b92d87bf499c5b17d1f0c
tmshn <shnch.tmr@gmail.com>
2019-01-12 19:49:21 +0900

    update tools

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,6 @@
 # Disable greeting
 set fish_greeting
 
-function ssh --description 'Wrapper of ssh to allow POSIX-shell style proxy commands'
-    set -l SHELL /bin/sh
-    /usr/bin/ssh $argv
-end
-
 pushd (dirname (status -f))
     source tools.fish
     source prompt.fish
