edee58ee5de4f7f67925c3dba2929304cdc5dd26
tmshn <shnch.tmr@gmail.com>
2019-01-12 19:49:21 +0900

    update tools

--- config/fish/tools.fish
+++ config/fish/tools.fish
@@ -1,25 +1,43 @@
 set -x PATH "$HOME/bin" $PATH
-set -x GOPATH $HOME
+
+# ==========================================================
+# Aliases/tools
+# ----------------------------------------------------------
+alias ssh 'set -l SHELL /bin/sh; and /usr/bin/ssh'
 
 set -x EDITOR micro
 
 set -x FZF_DEFAULT_OPTS '--border --cycle --height 30% --layout reverse --exit-0 --select-1'
 
 alias cat bat
-alias goto 'cd (ghq root)/(ghq list | fzf)'
 alias ll 'exa --git --long --group --classify --header --tree --level=1 --created --modified --accessed --time-style=iso'
 
+function cdrepo --description 'Choose git repo and cd to its root'
+    set repo (ghq list | fzf -q "$argv")
+    test -n "$repo"; and cd (ghq root)/$repo
+end
+
+# ==========================================================
+# Languages
+# ----------------------------------------------------------
+# Go
+set -x GOPATH $HOME
+
+# Anyenv
 set -x PATH "$HOME/.anyenv/bin" $PATH
 status --is-interactive; and source (anyenv init -|psub)
 
 # Pipenv
 set -x PIPENV_VENV_IN_PROJECT true
 
+# ==========================================================
 # Brewed tools
+# ----------------------------------------------------------
+# Openssl
 set -x PATH "/usr/local/opt/openssl/bin" $PATH
-set -x PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
-set -x PATH "/usr/local/opt/findutils/libexec/gnubin" $PATH
-set -x PATH "/usr/local/opt/gnu-sed/libexec/gnubin" $PATH
-set -x PATH "/usr/local/opt/gnu-tar/libexec/gnubin" $PATH
-set -x PATH "/usr/local/opt/gnu-time/libexec/gnubin" $PATH
-set -x PATH "/usr/local/opt/grep/libexec/gnubin" $PATH
+
+# GNU compaliants
+for t in coreutils findutils gnu-sed gnu-tar gnu-time grep
+    set -x PATH "/usr/local/opt/$t/libexec/gnubin" $PATH
+    set -x MANPATH "/usr/local/opt/$t/libexec/gnuman" $MANPATH
+end
