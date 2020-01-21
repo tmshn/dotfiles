0517ea00c60c55eeb4ae29fba35f07a72965839e
tmshn <shnch.tmr@gmail.com>
2020-01-21 17:29:08 +0900

    update

--- config/fish/tools.fish
+++ config/fish/tools.fish
@@ -14,49 +14,79 @@ function addpath
     end
 end
 
+
+addpath "$XDG_CONFIG_HOME/../bin"
 addpath "$HOME/bin"
 
 # ==========================================================
 # Aliases/tools
 # ----------------------------------------------------------
 alias ssh 'set -l SHELL /bin/sh; command ssh'
+alias scp 'set -l SHELL /bin/sh; command scp'
 
-set -x EDITOR kak
+alias dif 'git diff --no-index --color'
+
+set -x EDITOR vi
 
 set -x FZF_DEFAULT_OPTS '--border --cycle --height 30% --layout reverse --exit-0 --select-1'
 
-alias cat bat
+# alias cat bat
 alias ll 'exa --git --long --group --classify --header --tree --level=1 --created --modified --accessed --time-style=iso'
 
-function __cd_candidate
-    function __label_cd_list
-        sed -E (printf 's#^%s/#~/#; s/$/\\\\x0 \\033[93m(%s)\\033[0m/' "$HOME" "$argv")
-    end
-
-    begin
-        ghq list --full-path                            | __label_cd_list GHQ
-        git rev-parse --show-cdup 2> /dev/null | grep . | __label_cd_list Repo root
-        find . -maxdepth 1 -mindepth 1 -type d          | __label_cd_list Current dir
-        echo -s $dirprev\n | grep . | sort -u           | __label_cd_list History
-    end | fzf --no-multi --query="$argv" --ansi \
-        | cut -d '' -f 1 | string escape --no-quoted | sed -E 's/^\\\\~/~/'
+function __label_candidate
+    sort -u | sed -E "s/\$/\x00$argv/"
 end
 
 function __my_complete
-    set -l _argv (commandline -o)
-    set -l _cmd $_argv[1]
-    set -e _argv[1]
-    switch "$_cmd"
-        case 'cd'
-            commandline (printf 'cd %s' (__cd_candidate $_argv))
+    set -l command (commandline -po)
+    set -l query (commandline -t)
+    set -l result (
+        begin
+            # Custom completion for each command
+            switch "$command[1]"
+                case cd pushd
+                    ghq list --full-path | sed -E "s#^$HOME#~#" | __label_candidate GHQ
+                    git rev-parse --show-cdup 2> /dev/null      | __label_candidate Repo root
+                    echo -s $dirprev\n | sed -E "s#^$HOME#~#"   | __label_candidate History
+                    # Default completion
+                    complete -C(commandline -p) | sort -u | tr '\t' '\0' | __label_candidate ' (completion)'
+                case start
+                    ghq list
+                case docker
+                    docker image ls --format='{{ .Repository }}:{{ .Tag }}\t{{ .Size }} created {{ .CreatedSince }}' | tr '\t' '\0'
+                    # Default completion
+                    complete -C(commandline -p) | sort -u | tr '\t' '\0' | __label_candidate ' (completion)'
+            end
+        end | sed -E '/^(\x00|$)/d; s/\x00(.*)/\x00\t\x1B[36m\1\x1B[0m/' \
+            | fzf --multi --query="$query" --ansi \
+            | cut -d '' -f 1 \
+            | string escape --no-quoted \
+            | sed -E 's/^\\\\~/~/'
+    )
+    if [ (count $result) -gt 0 ]
+        commandline -t "$result"
+    end
+    commandline -f repaint
+end
+
+function fzf_complete -d 'fzf-version of complete'
+    set -l choosen (complete -C | sed -E 's/(\t.*)/\x1B[36m\1\x1B[0m/' \
+                                | fzf --ansi --exit-0 --select-1 \
+                                | cut -d \t -f 1 \
+                                | string escape --no-quoted \
+                                | sed -E 's/^\\\\~/~/')
+    if [ (count $choosen) -gt 0 ]
+        commandline -t "$choosen"
     end
     commandline -f repaint
 end
 
 function fish_user_key_bindings
-    bind \t forward-char
-    bind \t\t complete
     bind -k btab __my_complete
+end
+
+function start
+    cd (start.sh $argv)
 end
 
 # ==========================================================
@@ -64,6 +94,11 @@ end
 # ----------------------------------------------------------
 # Go
 set -x GOPATH $HOME
+# ref:
+# - https://github.com/syndbg/goenv/pull/70
+# - https://github.com/syndbg/goenv/issues/72
+# set -x GOENV_DISABLE_GOPATH 1
+# set -x GOENV_DISABLE_GOROOT 1
 
 # Anyenv
 addpath "$HOME/.anyenv/bin"
@@ -71,6 +106,14 @@ status --is-interactive; and source (anyenv init -|psub)
 
 # Pipenv
 set -x PIPENV_VENV_IN_PROJECT true
+
+# Python Poetry
+addpath "$HOME/.poetry/bin"
+
+# Bundle
+set -x BUNDLE_PATH "vendor/bundle"
+
+set -x JSONNET_PATH "$HOME/src/github.com/grafana/grafonnet-lib/grafonnet/"
 
 # ==========================================================
 # Brewed tools
