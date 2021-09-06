b64f1b3603b69f28a2ded6f7bab6c9aaead6ada3
Shinichi TAMURA (tmshn) <hello@tmshn.dev>
2021-09-06 19:15:53 +0900

    simplify things

--- bin/start.sh
+++ bin/start.sh
@@ -8,25 +8,27 @@
 }
 
 : Parse target && {
-    if [[ "${1:?}" =~ ^(.+://)?(.*)$ ]]; then
+    if [[ "${1:-}" =~ ^(.+://)?(.+)$ ]]; then
         readonly schema=${BASH_REMATCH[1]:-https://}
-        readonly target="${BASH_REMATCH[2]:?}"
-        readonly work=$(test "${target%%/*}" = work && echo yes || echo no)
-        readonly abspath="$(ghq root)/${target}"
+        target=$(echo "${BASH_REMATCH[2]}" | sed -E 's#(\.git)?/?$##g')
+        readonly target
+        abspath="$(ghq root)/${target}"
+        readonly abspath
     else
-        # Never fallback here, but just in case...
+        "We need an argument!"
         exit 1
     fi
 }
 
 : Find current session && {
-    readonly current_pane=$(
+    current_pane=$(
         tmux list-panes -s -F '#{pane_id} #{pane_current_path}' \
         | grep -E " ${abspath}($|/)" \
         | sort \
         | head -n 1 \
         | cut -d' ' -f1
     )
+    readonly current_pane
     if [[ -n "${current_pane}" ]]; then
         echo "You already have a pane for this; let's go there!" >&2
         tmux switch-client -t "${current_pane}"
@@ -36,13 +38,11 @@
 }
 
 : Prepare directory && {
-    if [[ "${work}" = yes ]]; then
-        mkdir -p "${abspath}"
-    elif [[ -z "$(ghq list -e "${target}")" ]]; then
+    if [[ -z "$(ghq list -e "${target}")" ]]; then
         ghq get "${schema}${target}" >&2
         (
             cd "${abspath}"
-            if [[ "${abspath}" =~ github.com ]]; then
+            if [[ "${target}" =~ github.com && ! "${target}" =~ github.com/tmshn/ ]]; then
                 hub fork --remote-name myorigin >&2 || :
             fi
         )
